5b5b120f6e0cbcc50482c7489bbd8d3420bf10e5
= <ryaninternet62@gmail.com>
2022-01-02 14:00:21 -0300

    Add o serializable

--- Spring/src/main/java/br/com/kronosys/domain/Schema.java
+++ Spring/src/main/java/br/com/kronosys/domain/Schema.java
@@ -12,6 +12,8 @@ import javax.persistence.Table;
 @Entity @Table(name="hst01_schema")
 public class Schema implements Serializable
 {
+	private static final long serialVersionUID = 1L;
+
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long chave;
 	
