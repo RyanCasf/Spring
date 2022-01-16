7c683ebf4c67204145c0917750668f39f2638db5
= <ryaninternet62@gmail.com>
2022-01-16 19:23:41 -0300

    Adicionei uma API JSON no h2.

--- Spring/src/main/java/br/com/kronosys/Spring/VerificacaoBackupController.java
+++ Spring/src/main/java/br/com/kronosys/Spring/VerificacaoBackupController.java
@@ -2,6 +2,7 @@ package br.com.kronosys.Spring;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +14,8 @@ import br.com.kronosys.repository.CourseRepository;
 @RestController @RequestMapping("/api/schemas")
 public class VerificacaoBackupController 
 {
-	private final CourseRepository courseRepository = null;
+	@Autowired
+	private CourseRepository courseRepository;
 
     @GetMapping
     public @ResponseBody List<Schema> list() {
